e658517cc7c48be543d2ff6ab793c16406174b00
andrew <andrew.mikhailov.18.01@gmail.com>
2017-04-28 23:54:52 +0700

    Update sw.

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,6 @@ self.addEventListener('install', (event) => {
     caches.open('homepage').then((cache) => {
       return cache.addAll([
         '/',
-        '/mtg-life-counter/img/*.jpg',
         '/mtg-life-counter/img/add-player.png',
         '/mtg-life-counter/img/arrow.svg',
         '/mtg-life-counter/img/Art_M15_Graphic1.png',
@@ -22,7 +21,6 @@ self.addEventListener('install', (event) => {
         '/mtg-life-counter/img/sinchronize.png',
         '/mtg-life-counter/fonts/Beleren2016-Bold.woff',
         '/mtg-life-counter/css/main.css',
-        '/mtg-life-counter/static/js/bundle.js',
         '/manifest.json',
       ]);
     }).then(() => {
